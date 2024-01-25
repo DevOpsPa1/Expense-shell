ed90f8337011827192f533187737fcb3285181e7
Balubadi <balubadi.pavankumar@gmail.com>
2024-01-25 21:45:45 +0530

    expense-froentend

--- froentend.sh
+++ froentend.sh
@@ -1,15 +1,17 @@
+log_file=expense.log
+
 echo -e "\e[36m install nginx \e[0m"
-dnf install nginx -y
-
-cp expense.conf /etc/nginx/default.d/expense.conf
-
-rm -rf /usr/share/nginx/html/*
-
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-
-systemctl enable nginx
-systemctl start nginx
+dnf install nginx -y &>>log_file
+echo -e "\e[36m copy expense.conf file \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
+echo -e "\e[36m remove nginx defult content \e[0m"
+rm -rf /usr/share/nginx/html/* &>>log_file
+echo -e "\e[36m download the frontend file \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
+echo -e "\e[36m unzip frontend file \e[0m"
+cd /usr/share/nginx/html &>>log_file
+unzip /tmp/frontend.zip &>>log_file
+echo -e "\e[36m eanble and start nginx \e[0m"
+systemctl enable nginx &>>log_file
+systemctl start nginx &>>log_file
 #systemctl restart nginx.
