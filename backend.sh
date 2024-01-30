786db16a610d04ad736d30658e187b2977e1b8ca
Balubadi <balubadi.pavankumar@gmail.com>
2024-01-30 20:37:19 +0530

    expense-froentend

--- backend.sh
+++ backend.sh
@@ -14,35 +14,48 @@ else
 fi
 cp backend.service /etc/systemd/system/backend.service
 if [ $? -eq 0 ]; then
-  echo -e "\e[36m Sucesses \e[0m"
-else
   echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
 fi
 useradd expense
 if [ $? -eq 0 ]; then
-  echo -e "\e[36m Sucesses \e[0m"
-else
   echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
 fi
 
 mkdir /app
 if [ $? -eq 0 ]; then
-  echo -e "\e[36m Sucesses \e[0m"
-else
   echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
 fi
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
 
 cd /app
 npm install
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
 systemctl daemon-reload
 
 systemctl enable backend
 systemctl start backend
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
 dnf install mysql -y
 
 mysql -h 172.31.20.39 -uroot -pExpenseApp@1 < /app/schema/backend.sql
