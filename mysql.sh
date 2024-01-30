ea5d9286beb2b57bc04f0ef260e42f9a5f7b17c5
Balubadi <balubadi.pavankumar@gmail.com>
2024-01-30 20:53:48 +0530

    expense-froentend

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,32 @@
+echo -e "\e[36m Disable default mysql \e[0m"
 dnf module disable mysql -y
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
+echo -e "\e[36m coppy the mysq repo \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
+echo -e "\e[36m install mysql  \e[0m"
 dnf install mysql-community-server -y
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
+echo -e "\e[36m enable mysql \e[0m"
 systemctl enable mysqld
+echo -e "\e[36m Start mysql \e[0m"
 systemctl start mysqld
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
 
 mysql_secure_installation --set-root-pass ExpenseApp@1
