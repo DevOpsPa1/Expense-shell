b05420bd74a5200ba0773e496dee6f50fd221b1f
Balubadi <balubadi.pavankumar@gmail.com>
2024-01-30 21:22:59 +0530

    expense-froentend

--- backend.sh
+++ backend.sh
@@ -1,61 +1,69 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+log_file="/tmp/expense.log"
+echo -e "\e[36m Disable nodejs Default version \e[0m"
+dnf module disable nodejs -y &>>log_file
+echo -e "\e[36m Enable nodjs 18 version \e[0m"
+dnf module enable nodejs:18 -y &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m Sucesses \e[0m"
 fi
+echo -e "\e[36m Install nodejs \e[0m"
+dnf install nodejs -y &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
+echo -e "\e[36m Copy appliation file  \e[0m"
+cp backend.service /etc/systemd/system/backend.service &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
+echo -e "\e[36m Add user \e[0m"
+useradd expense &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
+echo -e "\e[36m Creating Application Dir \e[0m"
+mkdir /app &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
+echo -e "\e[36m Download Application \e[0m"
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
+echo -e "\e[36m Unzip the application \e[0m"
+cd /app &>>log_file
+unzip /tmp/backend.zip &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
+echo -e "\e[36m Insatll the Application \e[0m"
+cd /app &>>log_file
+npm install &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
+echo -e "\e[36m Enable nodjs 18 version \e[0m"
+systemctl daemon-reload &>>log_file
 
-dnf install nodejs -y
+systemctl enable backend &>>log_file
+systemctl start backend &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m Sucesses \e[0m"
 fi
-cp backend.service /etc/systemd/system/backend.service
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m Sucesses \e[0m"
-else
-  echo -e "\e[31m Sucesses \e[0m"
-fi
-useradd expense
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m Sucesses \e[0m"
-else
-  echo -e "\e[31m Sucesses \e[0m"
-fi
+dnf install mysql -y &>>log_file
 
-mkdir /app
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m Sucesses \e[0m"
-else
-  echo -e "\e[31m Sucesses \e[0m"
-fi
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
-cd /app
-unzip /tmp/backend.zip
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m Sucesses \e[0m"
-else
-  echo -e "\e[31m Sucesses \e[0m"
-fi
-
-cd /app
-npm install
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m Sucesses \e[0m"
-else
-  echo -e "\e[31m Sucesses \e[0m"
-fi
-systemctl daemon-reload
-
-systemctl enable backend
-systemctl start backend
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m Sucesses \e[0m"
-else
-  echo -e "\e[31m Sucesses \e[0m"
-fi
-dnf install mysql -y
-
-mysql -h 172.31.20.39 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.20.39 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
--- mysql.sh
+++ mysql.sh
@@ -1,32 +1,33 @@
+log_file="/tmp/expense.log"
 echo -e "\e[36m Disable default mysql \e[0m"
-dnf module disable mysql -y
+dnf module disable mysql -y &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m Sucesses \e[0m"
 fi
 echo -e "\e[36m coppy the mysq repo \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m Sucesses \e[0m"
 fi
 echo -e "\e[36m install mysql  \e[0m"
-dnf install mysql-community-server -y
+dnf install mysql-community-server -y &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m Sucesses \e[0m"
 fi
 echo -e "\e[36m enable mysql \e[0m"
-systemctl enable mysqld
+systemctl enable mysqld &>>log_file
 echo -e "\e[36m Start mysql \e[0m"
-systemctl start mysqld
+systemctl start mysqld &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m Sucesses \e[0m"
 fi
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>log_file
