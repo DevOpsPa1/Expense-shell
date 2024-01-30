71a72059572181a7eb0526638d57ef1832928f7b
Balubadi <balubadi.pavankumar@gmail.com>
2024-01-30 20:36:17 +0530

    expense-froentend

--- backend.sh
+++ backend.sh
@@ -1,14 +1,36 @@
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
+if [ $? -eq 0 ]; then
+  echo -e "\e[36m Sucesses \e[0m"
+else
+  echo -e "\e[32m Sucesses \e[0m"
+fi
 
 dnf install nodejs -y
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[36m Sucesses \e[0m"
+else
+  echo -e "\e[32m Sucesses \e[0m"
+fi
 cp backend.service /etc/systemd/system/backend.service
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[36m Sucesses \e[0m"
+else
+  echo -e "\e[32m Sucesses \e[0m"
+fi
 useradd expense
+if [ $? -eq 0 ]; then
+  echo -e "\e[36m Sucesses \e[0m"
+else
+  echo -e "\e[32m Sucesses \e[0m"
+fi
 
 mkdir /app
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[36m Sucesses \e[0m"
+else
+  echo -e "\e[32m Sucesses \e[0m"
+fi
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
--- froentend.sh
+++ froentend.sh
@@ -1,24 +1,52 @@
-log_file=expense.log
+log_file="/tmp/expense.log"
 
 echo -e "\e[36m install nginx \e[0m"
 dnf install nginx -y &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
 echo -e "\e[36m copy expense.conf file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
 echo -e "\e[36m remove nginx defult content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
 echo -e "\e[36m download the frontend file \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
 echo -e "\e[36m unzip frontend file \e[0m"
 cd /usr/share/nginx/html &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
 unzip /tmp/frontend.zip &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
 echo -e "\e[36m eanble and start nginx \e[0m"
 systemctl enable nginx &>>log_file
 systemctl start nginx &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m Sucesses \e[0m"
+fi
 #systemctl restart nginx.
