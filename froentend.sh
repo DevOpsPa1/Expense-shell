f48613b50cee72a6c330730e55e4153fa243a678
Balubadi <balubadi.pavankumar@gmail.com>
2024-02-01 20:08:13 +0530

    expense-froentend

--- froentend.sh
+++ froentend.sh
@@ -1,47 +1,42 @@
 log_file="/tmp/expense.log"
 
-echo -e "\e[36m install nginx \e[0m"
+echo -e "\e[36m Installing nginx \e[0m"
 dnf install nginx -y &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m failure \e[0m"
 fi
-echo -e "\e[36m copy expense.conf file \e[0m"
+echo -e "\e[36m Copy Expense Config file file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m failure \e[0m"
 fi
-echo -e "\e[36m remove nginx defult content \e[0m"
+echo -e "\e[36m Clean Old Nginx Content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m failure \e[0m"
 fi
-echo -e "\e[36m download the frontend file \e[0m"
+echo -e "\e[36m Download Frontend Application Code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m failure \e[0m"
 fi
-echo -e "\e[36m unzip frontend file \e[0m"
+echo -e "\e[36m Extract Downloaded Application Content \e[0m"
 cd /usr/share/nginx/html &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m Sucesses \e[0m"
-else
-  echo -e "\e[31m failure \e[0m"
-fi
 unzip /tmp/frontend.zip &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m failure \e[0m"
 fi
-echo -e "\e[36m eanble and start nginx \e[0m"
+echo -e "\e[36m Starting Nginx Service \e[0m"
 systemctl enable nginx &>>log_file
 systemctl start nginx &>>log_file
 if [ $? -eq 0 ]; then
