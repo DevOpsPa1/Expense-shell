e5bb45052c0f640be9d3f36cc35073278f9cbe8b
Balubadi <balubadi.pavankumar@gmail.com>
2024-02-01 20:57:25 +0530

    expense-froentend

--- backend.sh
+++ backend.sh
@@ -1,44 +1,74 @@
 log_file="/tmp/expense.log"
-echo -e "\e[36m Disable nodejs Default version \e[0m"
+if [ -z "$1" ]; then
+  echo Password Input Missing
+  exit
+fi
+MYSQL_ROOT_PASSWORD=$1
+echo -e "\e[36m Dsable NodeJS default Version \e[0m"
 dnf module disable nodejs -y &>>log_file
-echo -e "\e[36m Enable nodjs 18 version \e[0m"
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m failure \e[0m"
+fi
+echo -e "\e[36m Enable NodeJS 18 VErsion \e[0m"
 dnf module enable nodejs:18 -y &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m failure \e[0m"
 fi
-echo -e "\e[36m Install nodejs \e[0m"
+echo -e "\e[36m Install NodeJS \e[0m"
 dnf install nodejs -y &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m failure \e[0m"
 fi
-echo -e "\e[36m Copy appliation file  \e[0m"
+echo -e "\e[36m Copy backend Service File  \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m failure \e[0m"
 fi
-echo -e "\e[36m Add user \e[0m"
-useradd expense &>>log_file
+id expense &>>log_file
+if [ $? -ne 0 ]; then
+ echo -e "\e[36m Add Application User \e[0m"
+ useradd expense &>>log_file
+ if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+  else
+  echo -e "\e[31m failure \e[0m"
+  fi
+fi
+
+if [ ! -d /app ]; then
+  echo -e "\e[36m Creating Application Dir \e[0m"
+  mkdir /app &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m failure \e[0m"
 fi
-echo -e "\e[36m Creating Application Dir \e[0m"
-mkdir /app &>>log_file
+
+echo -e "\e[36m DElete old Application Content \e[0m"
+rm -rm /app/* &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m failure \e[0m"
 fi
-echo -e "\e[36m Download Application \e[0m"
+
+echo -e "\e[36m Download Application Content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
-echo -e "\e[36m Unzip the application \e[0m"
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m failure \e[0m"
+fi
+
+echo -e "\e[36m Extract Application content \e[0m"
 cd /app &>>log_file
 unzip /tmp/backend.zip &>>log_file
 if [ $? -eq 0 ]; then
@@ -46,17 +76,29 @@ if [ $? -eq 0 ]; then
 else
   echo -e "\e[31m failure \e[0m"
 fi
-echo -e "\e[36m Insatll the Application \e[0m"
-cd /app &>>log_file
+echo -e "\e[36m Download NodeJs Dependencies \e[0m"
 npm install &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Sucesses \e[0m"
 else
   echo -e "\e[31m failure \e[0m"
 fi
-echo -e "\e[36m Enable nodjs 18 version \e[0m"
+echo -e "\e[36m Install MySQL Clint to Schema \e[0m"
+dnf install mysql -y &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m failure \e[0m"
+fi
+echo -e "\e[36m Load Schema \e[0m"
+mysql -h 172.31.87.3 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Sucesses \e[0m"
+else
+  echo -e "\e[31m failure \e[0m"
+fi
+echo -e "\e[36m Starting Backend Service \e[0m"
 systemctl daemon-reload &>>log_file
-
 systemctl enable backend &>>log_file
 systemctl start backend &>>log_file
 if [ $? -eq 0 ]; then
@@ -64,6 +106,3 @@ if [ $? -eq 0 ]; then
 else
   echo -e "\e[31m failure \e[0m"
 fi
-dnf install mysql -y &>>log_file
-
-mysql -h 172.31.87.3 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
